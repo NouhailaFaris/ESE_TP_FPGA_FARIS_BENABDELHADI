c7142ab0f62f2ea63b2dd939ee2f670b580139bd
D060 <ese@ensea.fr>
2024-12-19 12:04:00 +0100

    Chenillard

--- TP_FPGA.vhd
+++ TP_FPGA.vhd
@@ -4,12 +4,13 @@ entity TP_FPGA is
 port (
 i_clk : in std_logic;
 i_rst_n : in std_logic;
-o_led : out std_logic
+o_led : out std_logic_vector(7 downto 0)
 );
 end entity TP_FPGA;
 architecture rtl of TP_FPGA is
 signal r_led_enable : std_logic := '0';
 signal r_led : std_logic := '0';
+signal r_chenille : std_logic_vector(7 downto 0) := "00000001";
 begin
 process(i_clk, i_rst_n)
 variable counter : natural range 0 to 5000000 := 0;
@@ -26,9 +27,10 @@ counter := counter + 1;
 r_led_enable <= '0';
 end if;
 if (r_led_enable = '1') then
-r_led <= not r_led;
+chenille(7 downto 1) <= chenille (6 downto 0);
+chenille(0) <= chenille(7);
 end if;
 end if;
 end process;
-o_led <= r_led;
+o_led <= chenille;
 end architecture rtl;
